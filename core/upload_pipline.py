c9e890ffce8ea7082a5c19076cfde344d784c57c
ptpyip <ptpyip@connect.ust.hk>
2024-01-29 16:22:44 +0800

    add upload_video_file to UploadPipeline

--- core/upload_pipline.py
+++ core/upload_pipline.py
@@ -1,5 +1,6 @@
 import os.path
 
+from .vec_db import SupabaseDB
 from .utils import video_processing
 from .utils import VideoExtractor
 from .module.segmenter import ShotDetectSegmenter
@@ -11,6 +12,40 @@ class UploadPipeline():
     segmenter = ShotDetectSegmenter(MOMENT_OUT_DIR, use_adaptive=True) 
     extractor = VideoExtractor()
     vectorizer = CLIPVectorizer()
+    db = SupabaseDB()
+    
+    def upload_video_file(self, video_path):
+        assert os.path.exists(video_path)
+        self.segmenter.split(video_path)        ## Split to MOMENT_OUT_DIR
+        
+        moment_datas = self.vectorize_moments() 
+                
+        ## upload 
+        
+        # create one moment
+        # then upload vectors fro each frame for that moment
+        for moment in moment_datas:
+            
+            res = self.db.insert(
+                table_name=self.MOMENT_TABLE_NAME,
+                data={"name": moment.get("name","")}
+            )
+                    
+            moment_id = res.data[0]["id"]       # may need chane when new db is used?
+            
+            frames = moment.get("frames", [])
+            features = moment.get("features", [])    
+            for frame, feature in zip(frames, features):
+                frame_base64 = video_processing.encode_img_to_base64(frame)
+                
+                res = self.db.insert(
+                    self.MOMENT_FEATURE_TABLE_NAME,
+                    data = {
+                        "moment_id": moment_id,
+                        "frame_base64": frame_base64.decode("utf-8"),           # need to turn byte to str, as JSON only accept str
+                        "vector": feature.tolist()                              # jsn only support list
+                    }
+                )
       
     def vectorize_moments(self, moment_dir=None):
         if moment_dir is None:
