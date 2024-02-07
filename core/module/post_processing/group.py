74889e966cd8028d4efe444693a2a43063f3ea25
ptpyip <ptpyip@connect.ust.hk>
2024-02-07 12:50:21 +0800

    update grouping

--- core/module/post_processing/group.py
+++ core/module/post_processing/group.py
@@ -1,5 +1,13 @@
 def group_frame2moments(feature_result):
     """
+    NO NEED
+    just use SQL GROUP BY
+    '''
+        SELECT moment_id, COUNT(*) AS number_of_frame, max(vector <-> '{input_vector.tolist()}') AS distance
+        FROM moment_feature_test
+        group by moment_id 
+        ORDER BY distance LIMIT {int(k)};
+    '''
     here assume feaure are vectorized from one frame.
     given a list of feature result:
     [
