d6a02b241a84457b03ec8b61bdfd2e998d8190e6
ptpyip <ptpyip@connect.ust.hk>
2024-02-08 20:27:02 +0800

    bug fix: vector not flatten

--- core/vec_db/pgvectorDB.py
+++ core/vec_db/pgvectorDB.py
@@ -9,6 +9,8 @@ def get_pg_db_url(server_url, port_num, db_name, user_name, pwd):
 
 class PgvectorDB: 
     PG_DB_CONNECTION = "postgresql://postgres.augwkisabfpevnwvzzqu:password_test_123123@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
+    # import torch
+    # torch.Tensor.squeeze
     
     def __init__(self, 
         server_url = "db.augwkisabfpevnwvzzqu.supabase.co", 
@@ -18,11 +20,10 @@ class PgvectorDB:
         # self.PG_DB_CONNECTION = get_pg_db_url(server_url, port_num, db_name, user_name, pwd)
         self.engine = create_engine(self.PG_DB_CONNECTION)
         self.Session = sessionmaker(self.engine)
-        
     def fetch_features_by_vector(self, input_vector, k=5, return_frame=False):
         with self.Session() as session:
             return session.execute(text(f"""
-                SELECT id AS feature_id, moment_id,{' frame_base64,' if return_frame else ''} vector <-> '{input_vector.tolist()}' AS distance
+                SELECT id AS feature_id, moment_id,{' frame_base64,' if return_frame else ''} vector <-> '{input_vector.squeeze().tolist()}' AS distance
                 FROM moment_feature_test 
                 ORDER BY distance LIMIT {int(k)};"""
                 # WHERE vector <-> '{input_vector.tolist()}' < {distance}"""
@@ -34,7 +35,7 @@ class PgvectorDB:
             return session.execute(text(f"""
                 SELECT moment_id,
                     {' COUNT(*) AS number_of_frame,' if return_frame_count else ''} 
-                    max(vector <-> '{input_vector.tolist()}') AS distance
+                    max(vector <-> '{input_vector.squeeze().tolist()}') AS distance
                 FROM moment_feature_test
                 group by moment_id 
                 ORDER BY distance LIMIT {int(k)};"""
