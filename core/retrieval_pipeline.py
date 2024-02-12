e9324e1b0c32787992e41bac29653bc5fd440070
ptpyip <yippaco123123@gmail.com>
2024-02-12 22:35:35 +0800

    update fetching code and SQL

--- core/retrieval_pipeline.py
+++ core/retrieval_pipeline.py
@@ -1,4 +1,3 @@
-
 from .vec_db import PgvectorDB
 from .module.vectorizer import CLIPVectorizer
 
@@ -9,7 +8,26 @@ class RetrievalPipeline:
     )
     vectorizer = CLIPVectorizer()
     
-    def retrive(self, prompt: str, k=5):
+    def retrive_moment_ids(self, prompt: str, k=5) -> list[int, float]:
         query_vec = self.vectorizer.vectorize_txt(prompt)
         
-        return self.db.fetch_moment_by_vector(query_vec)
+        return self.db.fetch_moment_id_by_vector(
+            "qvhiglight_clip_moment_vector_0209",
+            query_vec
+            )                        # [(id, dist)]
+
+    def retrive_moments(self, prompt: str, k=5):
+        query_vec = self.vectorizer.vectorize_txt(prompt)
+        
+        moments = self.db.fetch_moments_by_vector(
+            vector_table_name="qvhiglight_clip_moment_vector_0209",
+            moment_table_name="qvhiglight_clip_moment_0209",
+            input_vector=query_vec
+        ) 
+        
+        moment_scores = {
+            moment_id: 1 - distance for moment_id, distance in moments
+        }
+        
+        
+        
--- core/vec_db/pgvectorDB.py
+++ core/vec_db/pgvectorDB.py
@@ -16,30 +16,70 @@ class PgvectorDB:
         server_url = "db.augwkisabfpevnwvzzqu.supabase.co", 
         port_num = 5432, db_name = "postgres", 
         user_name = "postgres", pwd="password_test_123123"
-    ) -> None:
-        # self.PG_DB_CONNECTION = get_pg_db_url(server_url, port_num, db_name, user_name, pwd)
+    ):
+        self.PG_DB_CONNECTION = get_pg_db_url(server_url, port_num, db_name, user_name, pwd)
         self.engine = create_engine(self.PG_DB_CONNECTION)
         self.Session = sessionmaker(self.engine)
-    def fetch_features_by_vector(self, input_vector, k=5, return_frame=False):
+        
+    # def set_VMR_table(self, moment_table_name, )
+
+    def fetch_moments_by_vector(self, vector_table_name:str, moment_table_name: str, input_vector, k=5):
+        ## validate table_name
+        
+        with self.Session() as session:
+            
+            return session.execute(text(f"""
+                SELECT id, name, timestamp, min_distance
+                FROM {moment_table_name} AS moment_table
+                    JOIN (
+                        SELECT moment_id, min(distance) AS min_distance
+                        FROM (
+                            SELECT moment_id,
+                                vector <=> '{input_vector.squeeze().tolist()}' AS distance         
+                            FROM {vector_table_name}
+                            ORDER BY distance limit 100
+                        ) AS vector_table
+                        GROUP BY moment_id       
+                        ORDER BY min(distance) limit 5 
+                    ) AS result_table 
+                    ON result_table.moment_id = moment_table.id 
+            """)).fetchall()
+        
+    def fetch_features_by_vector(self, feature_table_name:str, input_vector, k=5, return_frame=False):
+        ## validate table_name
+        
         with self.Session() as session:
             return session.execute(text(f"""
-                SELECT id AS feature_id, moment_id,{' frame_base64,' if return_frame else ''} vector <-> '{input_vector.squeeze().tolist()}' AS distance
-                FROM moment_feature_test 
+                SELECT id AS feature_id, moment_id,{' frame_base64,' if return_frame else ''} vector <=> '{input_vector.squeeze().tolist()}' AS distance
+                FROM {feature_table_name} 
                 ORDER BY distance LIMIT {int(k)};"""
                 # WHERE vector <-> '{input_vector.tolist()}' < {distance}"""
             )).fetchall()
 
         
-    def fetch_moment_by_vector(self, input_vector, k=5, return_frame_count=False):
+    def fetch_moment_id_by_vector(self, feature_table_name:str, input_vector, k=5, return_frame_count=False):
         with self.Session() as session:
             return session.execute(text(f"""
                 SELECT moment_id,
                     {' COUNT(*) AS number_of_frame,' if return_frame_count else ''} 
-                    max(vector <-> '{input_vector.squeeze().tolist()}') AS distance
-                FROM moment_feature_test
+                    max(vector <=> '{input_vector.squeeze().tolist()}') AS distance
+                FROM {feature_table_name}
                 group by moment_id 
                 ORDER BY distance LIMIT {int(k)};"""
             )).fetchall()
+
+    def fetch_moment_by_id(self, moment_table_name: str, moment_id):
+        with self.Session() as session:
+            return session.execute(text(f"""
+                SELECT * FROM {moment_table_name} WHERE id = {moment_id}
+            """)).fetchall()
+            
+
+    def fetch_moment_by_ids(self, moment_table_name: str, moment_ids: tuple):
+        with self.Session() as session:
+            return session.execute(text(f"""
+                SELECT * FROM {moment_table_name} WHERE id IN {moment_ids}
+            """)).fetchall()
 
     def test_connection(self):
         with self.Session() as session:
