9a918c970cd6d721fb5533617077edf2c9d02080
ptpyip <yippaco123123@gmail.com>
2024-02-21 11:28:19 +0800

    add retrieval code for qvhighlight dataset

--- tool/eval/qvhighlight.py
+++ tool/eval/qvhighlight.py
@@ -10,6 +10,12 @@ def load_jsonl(file_path):
         for line in f.readlines():
             yield json.loads(line)
             
+def write_jsonl(dict_list, out_dir, file_name):
+     with open(path.join(out_dir, f"{file_name}.jsonl"), 'w') as out:
+        for data in dict_list:
+            josn_data = json.dumps(data) + '\n'
+            out.write(josn_data)
+            
 def upload_video_from_dataset(file_path, video_dir):
     up = UploadPipeline(
         moment_table_name="qvhiglight_clip_moment_0209",
@@ -28,6 +34,37 @@ def upload_video_from_dataset(file_path, video_dir):
         
         up.upload_video_file(video_path)
         # break
+
+def retrieve_video_using_dataset_prompt(file_path, k=5):
+    # DATASET_PATH = "/home/ptpyip/fyp/datasets/qvhilights/highlight_val_release.jsonl"
+    
+    rp = RetrievalPipeline()
+
+    result = []
+    for data in load_jsonl(file_path):        
+        retrieval_result = rp.retrieve_moments(data.get("query"), k=5)
+        
+        pred_relevant_windows = []
+        for moment_id, video_name, timestamp, cos_dist in retrieval_result:
+            pred_vid = rp.get_video_id(video_name)
+            print(pred_vid)
+            if pred_vid == data.get("vid"):
+                pred_relevant_windows.append(
+                    [*timestamp, 1 - cos_dist]
+                )
+        
+                
+        print(f"query: {data.get('qid')} has {len(pred_relevant_windows)} result")
+        result.append({
+            "qid": data.get("qid"),
+            "query": data.get("query"),
+            "vid":  data.get("vid"),
+            "pred_relevant_windows": pred_relevant_windows            
+        }) 
+          
+    
+    write_jsonl(result, "/home/ptpyip/fyp", "out")  
+        
     
 def test_retrieval(prompt):
     rp = RetrievalPipeline()
@@ -35,8 +72,9 @@ def test_retrieval(prompt):
     
 if __name__ == "__main__":
     print("hi")
-    DATA_PATH = "/data/ptpyip/downloads/datasets/qvhilights"
-    upload_video_from_dataset(f"{DATA_PATH}/highlight_val_release.jsonl", f"{DATA_PATH}/videos")
+    DATA_PATH = "/home/ptpyip/fyp/datasets/qvhilights"
+    # upload_video_from_dataset(f"{DATA_PATH}/highlight_val_release.jsonl", f"{DATA_PATH}/videos")
     # test_retrieval("Police in riot gear are marching down the street.")
+    retrieve_video_using_dataset_prompt(f"{DATA_PATH}/highlight_val_release.jsonl")
     
     
