78175f787d7a8c05d580b9f10a71c3abb545af6b
ptpyip <yippaco123123@gmail.com>
2024-04-06 00:35:22 +0800

    add CLI

--- tool/eval/qvhighlight.py
+++ tool/eval/qvhighlight.py
@@ -21,11 +21,13 @@ def write_jsonl(dict_list, out_dir, file_name):
             josn_data = json.dumps(data) + '\n'
             out.write(josn_data)
             
-def upload_video_from_dataset(file_path, video_dir):
-    up = UploadPipeline(
+def upload_video_from_dataset(
+        file_path, video_dir,
         moment_table_name="qvhiglight_clip_moment_0209",
-        vector_table_name="qvhiglight_clip_moment_vector_0209"
-    )
+        vector_table_name="qvhiglight_clip_moment_vector_0209",
+        use_moment_vector=False, upload_frame=True
+    ):
+    up = UploadPipeline(moment_table_name, vector_table_name, use_moment_vector, upload_frame)
     vids = {data["vid"] for data in load_jsonl(file_path)}   # use set to avoid repeat
     
     for vid in vids:
@@ -76,12 +78,26 @@ def test_retrieval(prompt):
     print(rp.retrive(str(prompt)))
     
 if __name__ == "__main__":
-    # print("hi")
-    # DATA_PATH = "/home/ptpyip/fyp/datasets/qvhilights"
-    # # upload_video_from_dataset(f"{DATA_PATH}/highlight_val_release.jsonl", f"{DATA_PATH}/videos")
+    DATA_PATH = "/csproject/dan3/data/qvhiglights/"
+    import argparse
+
+    parser = argparse.ArgumentParser("Test Upload Pipeline")
+    parser.add_argument("--moment-table", type=str)
+    parser.add_argument("--vector-table", type=str)
+    parser.add_argument("--use-moment-vector", action='store_true')
+    parser.add_argument("--video", type=str)
+    parser.add_argument("--source-dir", type=str, default=DATA_PATH)
+    args = parser.parse_args()
+    
+    upload_video_from_dataset(
+        file_path=f"{args.source_dir}/highlight_val_release.jsonl", 
+        video_dir=f"{args.source_dir}/videos",
+        moment_table_name=args.moment_table,
+        vector_table_name=args.vector_table
+    )
     # # test_retrieval("Police in riot gear are marching down the street.")
     # retrieve_video_using_dataset_prompt(f"{DATA_PATH}/highlight_val_release.jsonl")
-    
-    print(__path__)
+        
+    print("success")
     
     
