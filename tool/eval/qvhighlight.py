1a96091e138855947a965603c6d8263e4d55c220
ptpyip <yippaco123123@gmail.com>
2024-04-16 10:12:49 +0800

    add code to test retrieval time and refactor evaluation code

--- tool/eval/qvhighlight.py
+++ tool/eval/qvhighlight.py
@@ -10,68 +10,80 @@ from os import path
 
 from core import UploadPipeline, RetrievalPipeline
 from tool.utils import load_jsonl, write_jsonl
+from tool.datasets import QVHighlightsDataset, TVRFriendsDataset
+from .metric import eval_submission
 
-from .eval import eval_submission
+
+
+def upload_dataset(dataset):
+    pass
+
+def eval_retrieval(dataset):
+    pass
+
+
             
-def upload_video_from_dataset(file_path, video_dir):
-    up = UploadPipeline(
-        moment_table_name="qvhiglight_clip_moment_0209",
-        vector_table_name="qvhiglight_clip_moment_vector_0209"
-    )
-    vids = {data["vid"] for data in load_jsonl(file_path)}   # use set to avoid repeat
+# def upload_video_from_dataset(file_path, video_dir):
+#     up = UploadPipeline(
+#         moment_table_name="qvhiglight_clip_moment_0209",
+#         vector_table_name="qvhiglight_clip_moment_vector_0209"
+#     )
+#     vids = {data["vid"] for data in load_jsonl(file_path)}   # use set to avoid repeat
     
-    for vid in vids:
-        ## insert each video
-        print(vid)
+#     for vid in vids:
+#         ## insert each video
+#         print(vid)
         
-        video_path = path.join(video_dir, f"{vid}.mp4")        # aware of .avi
-        if not path.exists(path.join(file_path, video_path)):
-            print(f"video does not exixt: {path.join(file_path, video_path)}")
-            continue
+#         video_path = path.join(video_dir, f"{vid}.mp4")        # aware of .avi
+#         if not path.exists(path.join(file_path, video_path)):
+#             print(f"video does not exixt: {path.join(file_path, video_path)}")
+#             continue
         
-        up.upload_video_file(video_path)
-        # break
+#         up.upload_video_file(video_path)
+#         # break
 
-def retrieve_video_using_dataset_prompt(file_path, k=5, skip_not_exisit=True):
-    # DATASET_PATH = "/home/ptpyip/fyp/datasets/qvhilights/highlight_val_release.jsonl"
+# def retrieve_video_using_dataset_prompt(
+#     file_path, k=5, skip_not_exisit=True, eval=True, verbose = True,
+#     out_name="preds"
+# ):
+#     # DATASET_PATH = "/home/ptpyip/fyp/datasets/qvhilights/highlight_val_release.jsonl"
     
-    rp = RetrievalPipeline()
+#     rp = RetrievalPipeline()
 
-    gt = []
-    pred = []
-    for data in load_jsonl(file_path): 
-        vid = data.get("vid") 
+#     gt = []
+#     pred = []
+#     for data in load_jsonl(file_path): 
+#         vid = data.get("vid") 
         
-        retrieval_result = rp.retrieve_moments(data.get("query"), k=5, video_name=vid)
-        if len(retrieval_result) == 0 and skip_not_exisit:
-            # skip video does not exist.
-            continue
+#         retrieval_result = rp.retrieve_moments(data.get("query"), k=5, video_name=vid)
+#         if len(retrieval_result) == 0 and skip_not_exisit:
+#             # skip video does not exist.
+#             continue
         
-        gt.append(data)
-        pred_relevant_windows = []
-        for moment_id, video_name, timestamp, cos_dist in retrieval_result:
-            pred_vid = rp.get_video_id(video_name)
-            print(pred_vid)
-            if pred_vid == vid:
-                pred_relevant_windows.append(
-                    [*timestamp, 1 - cos_dist]
-                )
+#         gt.append(data)
+#         pred_relevant_windows = []
+#         for moment_id, video_name, timestamp, cos_dist in retrieval_result:
+#             pred_vid = rp.get_video_id(video_name)
+#             # print(pred_vid)
+#             if pred_vid == vid:
+#                 pred_relevant_windows.append(
+#                     [*timestamp, 1 - cos_dist]
+#                 )
+#         if verbose:                
+#             print(f"query: {data.get('qid')} has {len(pred_relevant_windows)} result")
+#         pred.append({
+#             "qid": data.get("qid"),
+#             "query": data.get("query"),
+#             "vid":  data.get("vid"),
+#             "pred_relevant_windows": pred_relevant_windows            
+#         })
+#     if eval:    
+#         results = eval_submission(pred, gt) 
         
-                
-        print(f"query: {data.get('qid')} has {len(pred_relevant_windows)} result")
-        pred.append({
-            "qid": data.get("qid"),
-            "query": data.get("query"),
-            "vid":  data.get("vid"),
-            "pred_relevant_windows": pred_relevant_windows            
-        })
-        
-    results = eval_submission(pred, gt) 
-        
-    # write_jsonl(result, "/home/ptpyip/fyp", "preds_metrics")  
-    
-    with open("/home/ptpyip/fyp/preds_metrics.json", "w") as f:
-        f.write(json.dumps(results, indent=4))
+#         with open("/home/ptpyip/fyp/preds_metrics.json", "w") as f:
+#             f.write(json.dumps(results, indent=4))
+#     else:        
+#         write_jsonl(pred, "/home/ptpyip/fyp", "preds.jsonl")  
 
         
     
@@ -84,6 +96,6 @@ if __name__ == "__main__":
     DATA_PATH = "/home/ptpyip/fyp/datasets/qvhighlights"
     # upload_video_from_dataset(f"{DATA_PATH}/highlight_val_release.jsonl", f"{DATA_PATH}/videos")
     # test_retrieval("Police in riot gear are marching down the street.")
-    retrieve_video_using_dataset_prompt(f"{DATA_PATH}/highlight_val_release.jsonl")
+    # retrieve_video_using_dataset_prompt(f"{DATA_PATH}/highlight_val_release.jsonl")
     
     
--- tool/test/retrieval_time/main.py
+++ tool/test/retrieval_time/main.py
@@ -7,10 +7,17 @@ using QVHiglight.
 4. Test SOTA ( MomentDETR, or even forzen ) 
 """
 import os
+import time
+import tqdm
+import clip
+import torch
+from sentence_transformers.util import cos_sim
+
 # from core import UploadPipeline, RetrievalPipeline
 from core.module.segmenter import ShotDetectSegmenter
 from core.utils import VideoExtractor
 from tool.utils import load_jsonl
+from tool.eval.qvhighlight import retrieve_video_using_dataset_prompt
 
 MOMENT_OUT_DIR = "test/proposal"
 
@@ -21,6 +28,8 @@ def test_naive_clip(
 ):
     segmenter = ShotDetectSegmenter(MOMENT_OUT_DIR, use_adaptive=True) 
     extractor = VideoExtractor()
+    vectorizer, _ = clip.load("ViT-B/16")
+
     video_dir = data_dir if video_dir is None else video_dir
     data_path = os.path.join(data_dir, "highlight_val_release.jsonl")
     assert os.path.exists(data_path)
@@ -40,12 +49,38 @@ def test_naive_clip(
         video_path = os.path.join(data_dir. video, f"{vid}.mp4")
         assert os.path.exists(video_path)
         
+        query = data.get("query")
+        query_vector = vectorizer.encode_text(query)
+        
         ## 1. segment moment
         segmenter.clean_dir(MOMENT_OUT_DIR)
         timestamp_list = segmenter.split(video_path)        ## Split to MOMENT_OUT_DIR
         
         ## 2. extract frames from vid
+        for segment_path, *timestamp in tqdm.tqdm(timestamp_list):
+            frames = extractor.extract_frames(segment_path)
+            
+            ### vectorize moment
+            frame_vectors = vectorizer.encode_image(frames) 
+            cos_scores = cos_sim(query_vector, frame_vectors)[0]
+            top_results = torch.topk(cos_scores, k=5)
+                       
+                
+        return
+    
+def test_rp():
+    start_time = time.time()
+    print(start_time)
+    retrieve_video_using_dataset_prompt("/home/ptpyip/fyp/datasets/qvhighlights/test_time.jsonl", eval=False, verbose = False,)
+    end_time = time.time()
+    print(end_time)
+
+    elapsed_time = end_time - start_time
+    print(f"Total execution time: {elapsed_time:.6f} seconds")
+    
+    
         
 if __name__ == "__main__":
     # test_naive_clip(number_of_query=15)
-    test_naive_clip(number_of_query=0.1)
+    # test_naive_clip(number_of_query=0.1)
+    test_rp()
